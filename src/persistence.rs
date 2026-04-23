bb36822e54e4afa6f3f4da97bf9acc734c25317d
Cyril Agustin <agustincyrilg@gmail.com>
2026-04-23 16:18:08 +0800

    Separate persistence and reorder task modules
    Extract persistence logic into a dedicated module and reorder the task
    model for better readability. Move file handling functions out of the
    task module to improve separation of concerns. Rearrange the contents of
    the task file to follow a top-down structure where core logic precedes
    trait implementations.

---
+++ src/persistence.rs
@@ -0,0 +1,168 @@
+use std::{error::Error, fmt, fs, io::ErrorKind};
+
+use crate::task::{Status, Task};
+
+pub fn load() -> Result<Vec<Task>, Box<dyn Error>> {
+    let json: String = match fs::read_to_string("tasks.json") {
+        Ok(content) => content,
+        Err(ref e) if e.kind() == ErrorKind::NotFound => String::new(),
+        Err(e) => return Err(Box::new(e)),
+    };
+    let tasks = read_json(json)?;
+    Ok(tasks)
+}
+
+pub fn save(tasks: Vec<Task>) -> Result<(), Box<dyn Error>> {
+    let path = "tasks.json";
+    let temp_path = "tasks.json.tmp";
+    let contents = tasks_to_json(tasks);
+    fs::write(temp_path, contents.as_bytes())?;
+    fs::rename(temp_path, path)?;
+    Ok(())
+}
+
+fn read_json(json: String) -> Result<Vec<Task>, FileError> {
+    let mut tasks: Vec<Task> = Vec::new();
+
+    if json.is_empty() {
+        return Ok(tasks);
+    }
+    let less_json = json
+        .strip_prefix(
+            r##"{
+    "tasks": [
+"##,
+        )
+        .ok_or(FileError::MissingTasks)?;
+
+    let key_strs = [
+        r##""id": "##,
+        r##",
+            "desc": ""##,
+        r##"",
+            "status": ""##,
+        r##"",
+            "created_at": ""##,
+        r##"",
+            "updated_at": ""##,
+        r##"""##,
+    ];
+
+    for obj in less_json
+        .trim_matches(|c| c == '[' || c == ']')
+        .split("},\n        {")
+    {
+        let task = obj.trim_matches(|c| c == '{' || c == '}');
+
+        let mut values = Vec::new();
+        for key in key_strs.windows(2) {
+            let value = find_json_value(task, key[0], key[1])?;
+            values.push(value.to_string());
+        }
+
+        tasks.push(Task {
+            id: values[0].parse::<u32>().unwrap(),
+            desc: values[1].clone(),
+            status: Status::try_from(values[2].as_str()).unwrap(),
+            created_at: values[3].parse().unwrap(),
+            updated_at: values[4].parse().unwrap(),
+        })
+    }
+
+    Ok(tasks)
+}
+
+// Returns value in between before key and after key in task json
+fn find_json_value(task: &str, before: &str, after: &str) -> Result<String, FileError> {
+    let start: usize = task.find(before).ok_or(FileError::InvalidTask)? + before.len();
+    let end = task.rfind(after).unwrap();
+    Ok(task[start..end].to_string())
+}
+
+fn tasks_to_json(tasks: Vec<Task>) -> String {
+    let json_tasks: Vec<String> = tasks
+        .iter()
+        .map(|t| {
+            t.to_json()
+                .lines()
+                .map(|line| format!("        {}", line))
+                .collect::<Vec<String>>()
+                .join("\n")
+        })
+        .collect();
+    format!(
+        r##"{{
+    "tasks": [
+{}
+    ]
+}}"##,
+        json_tasks.join(",\n")
+    )
+}
+
+#[derive(Debug, PartialEq)]
+pub enum FileError {
+    InvalidTask,
+    MissingTasks,
+}
+
+impl fmt::Display for FileError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            FileError::InvalidTask => write!(f, "Error: Inavlid Task found in json file"),
+            FileError::MissingTasks => write!(f, "Error: Tasks array is missing from json file"),
+        }
+    }
+}
+
+impl Error for FileError {}
+
+#[cfg(test)]
+mod test {
+
+    use crate::task::tasks_example;
+
+    use super::*;
+
+    static JSON_EXAMPLE: &str = r##"{
+    "tasks": [
+        {
+            "id": 1,
+            "desc": "take a break",
+            "status": "done",
+            "created_at": "2024-02-29T12:51:00+08:00[Asia/Manila]",
+            "updated_at": "2024-02-29T12:51:00+08:00[Asia/Manila]"
+        },
+        {
+            "id": 2,
+            "desc": "buy milk",
+            "status": "in-progress",
+            "created_at": "2024-02-29T12:51:00+08:00[Asia/Manila]",
+            "updated_at": "2024-02-29T12:51:00+08:00[Asia/Manila]"
+        },
+        {
+            "id": 3,
+            "desc": "go home",
+            "status": "todo",
+            "created_at": "2024-02-29T12:51:00+08:00[Asia/Manila]",
+            "updated_at": "2024-02-29T12:51:00+08:00[Asia/Manila]"
+        }
+    ]
+}"##;
+
+    #[test]
+    fn test_tasks_to_json() {
+        let tasks = tasks_example();
+        let actual = tasks_to_json(tasks);
+        let expected = JSON_EXAMPLE;
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_read_json() {
+        let json = JSON_EXAMPLE.to_string();
+        let actual = read_json(json);
+        let expected = tasks_example();
+        assert_eq!(actual, Ok(expected));
+    }
+}
--- src/task.rs
+++ src/task.rs
@@ -1,7 +1,14 @@
 use crate::config::CliError;
-use std::{convert, error::Error, fmt, fs, io::ErrorKind};
+use std::{convert, fmt};
 
 use jiff::{Unit, Zoned};
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum Status {
+    Done,
+    ToDo,
+    InProgress,
+}
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct Task {
@@ -13,7 +20,7 @@ pub struct Task {
 }
 
 impl Task {
-    fn to_json(&self) -> String {
+    pub fn to_json(&self) -> String {
         format!(
             r##"{{
     "id": {},
@@ -29,136 +36,14 @@ impl Task {
             self.updated_at.round(Unit::Second).unwrap()
         )
     }
-
-    fn tasks_to_json(tasks: Vec<Task>) -> String {
-        let json_tasks: Vec<String> = tasks
-            .iter()
-            .map(|t| {
-                t.to_json()
-                    .lines()
-                    .map(|line| format!("        {}", line))
-                    .collect::<Vec<String>>()
-                    .join("\n")
-            })
-            .collect();
-        format!(
-            r##"{{
-    "tasks": [
-{}
-    ]
-}}"##,
-            json_tasks.join(",\n")
-        )
-    }
-
-    pub fn load() -> Result<Vec<Self>, Box<dyn Error>> {
-        let json: String = match fs::read_to_string("tasks.json") {
-            Ok(content) => content,
-            Err(ref e) if e.kind() == ErrorKind::NotFound => String::new(),
-            Err(e) => return Err(Box::new(e)),
-        };
-        let tasks = Self::read_json(json)?;
-        Ok(tasks)
-    }
-
-    pub fn save(tasks: Vec<Self>) -> Result<(), Box<dyn Error>> {
-        let path = "tasks.json";
-        let temp_path = "tasks.json.tmp";
-        let contents = Self::tasks_to_json(tasks);
-        fs::write(temp_path, contents.as_bytes())?;
-        fs::rename(temp_path, path)?;
-        Ok(())
-    }
-
-    fn read_json(json: String) -> Result<Vec<Self>, FileError> {
-        let mut tasks: Vec<Self> = Vec::new();
-
-        if json.is_empty() {
-            return Ok(tasks);
-        }
-        let less_json = json
-            .strip_prefix(
-                r##"{
-    "tasks": [
-"##,
-            )
-            .ok_or(FileError::MissingTasks)?;
-
-        let key_strs = [
-            r##""id": "##,
-            r##",
-            "desc": ""##,
-            r##"",
-            "status": ""##,
-            r##"",
-            "created_at": ""##,
-            r##"",
-            "updated_at": ""##,
-            r##"""##,
-        ];
-
-        for obj in less_json
-            .trim_matches(|c| c == '[' || c == ']')
-            .split("},\n        {")
-        {
-            let task = obj.trim_matches(|c| c == '{' || c == '}');
-
-            let mut values = Vec::new();
-            for key in key_strs.windows(2) {
-                let value = Self::find_json_value(task, key[0], key[1])?;
-                values.push(value.to_string());
-            }
-
-            tasks.push(Task {
-                id: values[0].parse::<u32>().unwrap(),
-                desc: values[1].clone(),
-                status: Status::try_from(values[2].as_str()).unwrap(),
-                created_at: values[3].parse().unwrap(),
-                updated_at: values[4].parse().unwrap(),
-            })
-        }
-
-        Ok(tasks)
-    }
-
-    // Returns value in between before key and after key in task json
-    fn find_json_value(task: &str, before: &str, after: &str) -> Result<String, FileError> {
-        let start: usize = task.find(before).ok_or(FileError::InvalidTask)? + before.len();
-        let end = task.rfind(after).unwrap();
-        Ok(task[start..end].to_string())
-    }
 }
 
-#[derive(Debug, PartialEq)]
-enum FileError {
-    InvalidTask,
-    MissingTasks,
-}
-
-impl Error for FileError {}
-
-impl fmt::Display for FileError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+impl Status {
+    fn as_str(&self) -> &'static str {
         match self {
-            FileError::InvalidTask => write!(f, "Inavlid Task"),
-            FileError::MissingTasks => write!(f, "Missing Tasks"),
-        }
-    }
-}
-
-#[derive(Debug, PartialEq, Clone)]
-pub enum Status {
-    Done,
-    ToDo,
-    InProgress,
-}
-
-impl fmt::Display for Status {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Status::Done => write!(f, "Done"),
-            Status::ToDo => write!(f, "Todo"),
-            Status::InProgress => write!(f, "In-Progress"),
+            Self::ToDo => "todo",
+            Self::InProgress => "in-progress",
+            Self::Done => "done",
         }
     }
 }
@@ -176,12 +61,12 @@ impl convert::TryFrom<&str> for Status {
     }
 }
 
-impl Status {
-    fn as_str(&self) -> &'static str {
+impl fmt::Display for Status {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            Self::ToDo => "todo",
-            Self::InProgress => "in-progress",
-            Self::Done => "done",
+            Status::Done => write!(f, "Done"),
+            Status::ToDo => write!(f, "Todo"),
+            Status::InProgress => write!(f, "In-Progress"),
         }
     }
 }
@@ -217,52 +102,4 @@ pub(crate) fn tasks_example() -> Vec<Task> {
             updated_at: time,
         },
     ]
-}
-
-#[cfg(test)]
-mod test {
-
-    use super::*;
-
-    static JSON_EXAMPLE: &str = r##"{
-    "tasks": [
-        {
-            "id": 1,
-            "desc": "take a break",
-            "status": "done",
-            "created_at": "2024-02-29T12:51:00+08:00[Asia/Manila]",
-            "updated_at": "2024-02-29T12:51:00+08:00[Asia/Manila]"
-        },
-        {
-            "id": 2,
-            "desc": "buy milk",
-            "status": "in-progress",
-            "created_at": "2024-02-29T12:51:00+08:00[Asia/Manila]",
-            "updated_at": "2024-02-29T12:51:00+08:00[Asia/Manila]"
-        },
-        {
-            "id": 3,
-            "desc": "go home",
-            "status": "todo",
-            "created_at": "2024-02-29T12:51:00+08:00[Asia/Manila]",
-            "updated_at": "2024-02-29T12:51:00+08:00[Asia/Manila]"
-        }
-    ]
-}"##;
-
-    #[test]
-    fn test_tasks_to_json() {
-        let tasks = tasks_example();
-        let actual = Task::tasks_to_json(tasks);
-        let expected = JSON_EXAMPLE;
-        assert_eq!(actual, expected);
-    }
-
-    #[test]
-    fn test_read_json() {
-        let json = JSON_EXAMPLE.to_string();
-        let actual = Task::read_json(json);
-        let expected = tasks_example();
-        assert_eq!(actual, Ok(expected));
-    }
 }
